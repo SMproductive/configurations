10c190ece0088e5eb083f4d45e6341dfd32eef4b
SMproductive <SMproductive@tutanota.de>
2021-07-08 13:24:49 +0200

    added aland removed st

--- dwmConfig.h
+++ dwmConfig.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char mute[] = "pactl set-sink-mute 0 toggle";
 static const char louder[] = "pactl set-sink-volume 0 +5%";
 static const char quieter[] = "pactl set-sink-volume 0 -5%";
